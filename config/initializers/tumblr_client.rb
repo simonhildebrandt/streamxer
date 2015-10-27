be52eab7a3c6ff46218b74a2b22217f4e23def3d
Simon Hildebrandt <simonhildebrandt@gmail.com>
2015-10-27 12:08:04 +1100

    Initial commit

--- config/initializers/tumblr_client.rb
+++
@@ -1,6 +0,0 @@
-Tumblr.configure do |config|
-  config.consumer_key = "S0m214hAfVeDgyq4BJOsAMzHxQ55FC88gTAySCiPPcPwvzZLrb"
-  config.consumer_secret = "XgLrHCOjt427WA8SxMchPzHO2iQ6Vty5p1zvb7g7YXR3SFlWUT"
-  # config.oauth_token = "kKMIsItkFq7t0SVSdkqCLPc5R1wqHzJbgJnGwQL8Nh6mWVeTBR"
-  # config.oauth_token_secret = "y385qgjo4frlauzGo6axhqwGwPwV7RAgmni6ZOUf5HfJTMM4ii"
-end
