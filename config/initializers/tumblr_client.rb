4360845bdd1fc8f7b8e7b23dc8d9879264d61cbc
Simon Hildebrandt <simonhildebrandt@gmail.com>
2015-12-10 15:15:22 +1100

    Added support for rails config files

--- config/initializers/tumblr_client.rb
+++ config/initializers/tumblr_client.rb
@@ -1,4 +1,6 @@
 Tumblr.configure do |config|
-  config.consumer_key = ENV['TUMBLR_KEY']
-  config.consumer_secret = ENV['TUMBLR_SECRET']
+  Rails.application.config_for(:tumblr).tap do |data|
+    config.consumer_key = data['consumer_key']
+    config.consumer_secret = data['consumer_secret']
+  end
 end
